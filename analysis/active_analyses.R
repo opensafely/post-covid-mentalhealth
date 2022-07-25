3839deb552b4a2bd18faec932c5696df62acf15d
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-07-25 10:27:49 +0100

    Depression
    Main, prescription, primary and secondary care

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -77,13 +77,15 @@ for (i in 1:length(outcomes)) {
 #df[c(2:21),1] <- FALSE
 
 #Run main MH outcomes:
-df[c(2:4,6:8,13:15,17:18,21),1] <- FALSE
+#df[c(2:4,6:8,13:15,17:18,21),1] <- FALSE
 
 #prescriptions: depression, anxiety, serious mental illness, and addiction
 #df[c(3:4,7:11,14:19),1] <- FALSE
 
 #Depression: main, prescription, primary and secondary care
-#df[c(5:21),1] <- FALSE
+df[c(5:21),1] <- FALSE
+df[c(2:4),c(8:10)] <- FALSE
+df[1,8] <- FALSE
 #Anxiety: main, prescription, primary and secondary care
 #df[c(1:4,9:21),1] <- FALSE
 #Serious mental illness
