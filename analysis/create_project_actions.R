49a02128e95f632340dab0c990fa5b892f21d1eb
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-03-31 11:49:13 +0100

    Fix typo on venn actions

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -238,8 +238,8 @@ venn <- function(cohort){
       needs = c(as.list(glue("preprocess_data_{cohort}")),
                 as.list(paste0(glue("make_model_input-cohort_{cohort}-main-"),venn_outcomes))),
       moderately_sensitive = list(
-        table2 = glue("output/venn_{cohort}.csv"),
-        table2_rounded = glue("output/venn_{cohort}_rounded.csv")
+        venn = glue("output/venn_{cohort}.csv"),
+        venn_rounded = glue("output/venn_{cohort}_rounded.csv")
       )
     )
   )
