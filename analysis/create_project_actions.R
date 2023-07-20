edd3d45f365d0831046e606348520d2d49471191
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-07-20 14:04:41 +0100

    Add prefix to needs

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -400,7 +400,7 @@ actions_list <- splice(
   action(
     name = "make_aer_input",
     run = "r:latest analysis/make_aer_input.R",
-    needs = as.list(active_analyses[grepl("-main-",active_analyses$name),]$name),
+    needs = as.list(paste0("cox_ipw-",active_analyses[grepl("-main-",active_analyses$name),]$name)),
     moderately_sensitive = list(
       aer_input = glue("output/aer_input-main.csv"),
       aer_input_rounded = glue("output/aer_input-main-rounded.csv")
