327f77582ea5e866b2ba02fe96838712acb3eb9a
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-08-08 15:05:57 +0100

    Tidy comments

--- analysis/post_release/fn-lifetable.R
+++ analysis/post_release/fn-lifetable.R
@@ -22,20 +22,18 @@ lifetable <- function(model_output, aer_input) {
                           days = c(0:(max(model_output$time_period_end)-1)),
                           stringsAsFactors = FALSE)
   
-  # Step 1. Average daily incidence in unexposed -------------------------------
-  print('Step 1. Average daily incidence in unexposed')
-  
-  # Divide the number of events in the unexposed by time in days
-  
+  # Step 1. Average daily incidence of the outcome in the unexposed ------------
+  print('Step 1. Average daily incidence of the outcome in the unexposed')
+
   lifetable$incidence_unexp <- aer_input$unexposed_events / aer_input$unexposed_person_days
  
-  # Step 2. Cumulative risk over time in unexposed -----------------------------
-  print('Step 2. Cumulative risk over time in unexposed')
+  # Step 2. Cumulative risk over time in the unexposed -------------------------
+  print('Step 2. Cumulative risk over time in the unexposed')
   
   lifetable$cumulative_survival_unexp <- cumprod(1 - lifetable$incidence_unexp) 
   
-  # Step 3. Add HRs  -----------------------------------------------------------
-  print('Step 3. Add HRs')
+  # Step 3. Add hazard ratios --------------------------------------------------
+  print('Step 3. Add hazard ratios')
 
   lifetable$hr <- NA
   
@@ -44,18 +42,14 @@ lifetable <- function(model_output, aer_input) {
     lifetable$hr <- ifelse(lifetable$days >= tmp$time_period_start & lifetable$days < tmp$time_period_end, tmp$hr, lifetable$hr)
   }
   
-  # Step 4. Predict incidence of outcome after exposure ------------------------
-  print('Step 4. Predict incidence of outcome after exposure')
-  
-  # Multiply daily incidence in unexposed by relevant hazard ratio
+  # Step 4. Predict the expected cumulative survival in the exposed ------------
+  print('Step 4. Predict the expected cumulative survival in the exposed')
   
   lifetable$cumulative_survival_exp <- cumprod(1 - (lifetable$hr * lifetable$incidence_unexp))
 
-  # Step 5. Calculate daily excess risk ----------------------------------------
-  print('Step 5. Calculate daily excess risk')
+  # Step 5. Calculate the daily excess risk ------------------------------------
+  print('Step 5. Calculate the daily excess risk')
 
-  # This is the difference in cumulative survival unexposed and expected cumulative survival in unexposed
-  
   lifetable$cumulative_difference_absolute_excess_risk <- lifetable$cumulative_survival_unexp - lifetable$cumulative_survival_exp
 
   # Return output for compiling ------------------------------------------------
