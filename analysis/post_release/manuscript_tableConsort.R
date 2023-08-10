bdb880e659537d92cd0c53c0662f3150d8b2907d
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-08-10 09:15:40 +0100

    Create manuscript_tableConsort.R

---
+++ analysis/post_release/manuscript_tableConsort.R
@@ -0,0 +1,28 @@
+# Load data --------------------------------------------------------------------
+print("Load data")
+
+df <- readr::read_csv(paste0(release,"consort_output_rounded.csv"),
+                      show_col_types = FALSE)
+
+# Filter data ------------------------------------------------------------------
+print("Filter data")
+
+df$removed <- NULL
+
+# Pivot table ------------------------------------------------------------------
+print("Pivot table")
+
+df <- df %>%
+  dplyr::group_by(Description,cohort) %>%
+  dplyr::mutate(N = min(N)) %>%
+  dplyr::ungroup() %>%
+  unique()
+
+df <- tidyr::pivot_wider(df, 
+                         names_from = "cohort",
+                         values_from = c("N"))
+
+# Save table -------------------------------------------------------------------
+print("Save table")
+
+readr::write_csv(df, "output/post_release/tableConsort.csv", na = "-")
