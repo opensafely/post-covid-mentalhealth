d031c7a1d7a51e3c9a04c42b90e64a3427a89932
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-08-24 16:35:56 +0100

    Add specify paths

---
+++ analysis/specify_paths_example.R
@@ -0,0 +1,12 @@
+# This file is used to specify paths. It is in the .gitignore to keep your information secret.
+# To use, please remove "_example" from the file name and add your specific file paths below.
+
+release <- "" # Specify path to release directory  
+path_aer_input <- paste0(release,"20230807/aer_input-main-rounded.csv")
+path_consort <- paste0(release,"20230807/consort_output_rounded.csv")
+path_median_iqr_age <- paste0(release,"20230807/median_iqr_age.csv")
+path_model_output <- paste0(release,"20230807/model_output_rounded.csv")
+path_table1 <- paste0(release,"20230807/table1_output_rounded.csv")
+path_table2 <- paste0(release,"20230807/table2_output_rounded.csv")
+path_venn <- paste0(release,"20230807/venn_output_rounded.csv")
+path_extendedtable1 <- paste0(release,"20230810/extendedtable1_output_rounded.csv")
