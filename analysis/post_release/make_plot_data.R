914fb22ee9830c7b3e39f6be198fd1c3287aee6a
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-11-06 13:12:30 +0000

    Prefer stata output

--- analysis/post_release/make_plot_data.R
+++ analysis/post_release/make_plot_data.R
@@ -1,5 +1,5 @@
-# Load data --------------------------------------------------------------------
-print('Load data')
+# Load model output ------------------------------------------------------------
+print('Load model output')
 
 df <- readr::read_csv(path_model_output,
                       show_col_types = FALSE)
@@ -35,6 +35,25 @@ print('Remove unsuccessful models')
 
 tmp <- unique(df[df$conf_high==Inf & grepl("day",df$term),]$name)
 df <- df[!(df$name %in% tmp),]
+
+# Load stata model output ------------------------------------------------------
+print('Load stata model output')
+
+tmp <- readr::read_csv(path_stata_model_output,
+                       show_col_types = FALSE)
+
+df$rank <- 0
+tmp$rank <- 1
+
+tmp <- tmp[,colnames(df)]
+df <- rbind(df,tmp)
+
+df <- df %>%
+  dplyr::group_by(name) %>%
+  dplyr::top_n(1, rank) %>%
+  dplyr::ungroup()
+
+df$rank <- NULL
 
 # Save plot data ---------------------------------------------------------------
 print('Save plot data')
