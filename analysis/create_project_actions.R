e72545be7093f8a0b1f33c8baa15a6c158d1c8df
Venexia Walker <venexia.walker@bristol.ac.uk>
2024-02-28 15:58:52 +0000

    Update make_model_output to include stata models

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -486,8 +486,8 @@ actions_list <- splice(
   action(
     name = "make_model_output",
     run = "r:latest analysis/make_model_output.R",
-    needs = as.list(paste0("cox_ipw-",setdiff(active_analyses$name,stata$name)),
-                    paste0("stata_cox_ipw-",stata$name)),
+    needs = as.list(c(paste0("cox_ipw-",setdiff(active_analyses$name,stata$name)),
+                    paste0("stata_cox_ipw-",stata$name))),
     moderately_sensitive = list(
       model_output = glue("output/model_output.csv"),
       model_output_midpoint6 = glue("output/model_output_midpoint6.csv")
